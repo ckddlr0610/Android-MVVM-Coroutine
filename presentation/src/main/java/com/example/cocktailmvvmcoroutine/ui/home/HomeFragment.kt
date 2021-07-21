11ff6208a313c1a1d11558eeb78e6369e9562167
Teddy_changik <aackddlr@gmail.com>
2021-07-21 12:50:53 +0900

    Navigation Component 세팅

---
+++ presentation/src/main/java/com/example/cocktailmvvmcoroutine/ui/home/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.example.cocktailmvvmcoroutine.ui.home
+
+import androidx.fragment.app.Fragment
+import com.example.cocktailmvvmcoroutine.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+}
