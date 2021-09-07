c122a9fbdaf39eec6b73bdc9c706e2dfb7f72a42
Teddy_changik <aackddlr@gmail.com>
2021-09-07 15:47:33 +0900

    Add DiskCacheStrategy to imageDecoding

--- app/src/main/java/com/example/cocktailmvvmcoroutine/ui/adapter/ViewBindingAdapter.kt
+++ app/src/main/java/com/example/cocktailmvvmcoroutine/ui/adapter/ViewBindingAdapter.kt
@@ -3,12 +3,14 @@ package com.example.cocktailmvvmcoroutine.ui.adapter
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
 @BindingAdapter("imageFromUrl")
 fun bindImageFromUrl(view: ImageView, imageUrl: String) {
     Glide.with(view.context)
         .load(imageUrl)
+        .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
         .transition(DrawableTransitionOptions.withCrossFade())
         .into(view)
 }
