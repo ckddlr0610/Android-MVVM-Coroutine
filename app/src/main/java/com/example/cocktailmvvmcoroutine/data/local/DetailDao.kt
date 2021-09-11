ec4b6944aac96abef0d7850897e1a0b9f9b27cf4
Teddy_changik <aackddlr@gmail.com>
2021-09-11 18:11:46 +0900

    DetailDao 함수 추가

--- app/src/main/java/com/example/cocktailmvvmcoroutine/data/local/DetailDao.kt
+++ app/src/main/java/com/example/cocktailmvvmcoroutine/data/local/DetailDao.kt
@@ -1,7 +1,22 @@
 package com.example.cocktailmvvmcoroutine.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.cocktailmvvmcoroutine.data.model.CocktailDetailInfo
+import com.example.cocktailmvvmcoroutine.data.model.Ingredient
 
 @Dao
 interface DetailDao {
+    @Insert
+    fun insertCocktailDetailInfo(cocktailDetailInfo: CocktailDetailInfo)
+
+    @Query("SELECT * FROM CocktailDetailInfo WHERE strDrink = :strDrink")
+    fun getCocktailDetailInfo(strDrink: String): CocktailDetailInfo
+
+    @Insert
+    fun insertIngredient(ingredient: Ingredient)
+
+    @Query("SELECT * FROM Ingredient WHERE strIngredient = :strIngredient")
+    fun getIngredient(strIngredient: String): Ingredient
 }
