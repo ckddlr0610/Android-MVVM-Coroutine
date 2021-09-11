7be6548ff3191e27eabb783fc50d89e4fd5a205c
Teddy_changik <aackddlr@gmail.com>
2021-09-11 13:14:47 +0900

    Add DetailCocktailInfo, IngredientInfo API Call

--- app/src/main/java/com/example/cocktailmvvmcoroutine/data/network/CocktailService.kt
+++ app/src/main/java/com/example/cocktailmvvmcoroutine/data/network/CocktailService.kt
@@ -1,6 +1,8 @@
 package com.example.cocktailmvvmcoroutine.data.network
 
+import com.example.cocktailmvvmcoroutine.data.model.CocktailDetailInfo
 import com.example.cocktailmvvmcoroutine.data.model.CocktailResponse
+import com.example.cocktailmvvmcoroutine.data.model.Ingredient
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -9,4 +11,14 @@ interface CocktailService {
     suspend fun getAlcoholicCocktailList(
         @Query("a") alcoholic: String = "Alcoholic"
     ): CocktailResponse
+
+    @GET("api/json/v1/1/lookup.php?")
+    suspend fun getCocktailDetailInfo(
+        @Query("i") idDrink: Long
+    ): CocktailDetailInfo
+
+    @GET("api/json/v1/1/lookup.php?")
+    suspend fun getIngredient(
+        @Query("iid") idIngredient: Long
+    ): Ingredient
 }
